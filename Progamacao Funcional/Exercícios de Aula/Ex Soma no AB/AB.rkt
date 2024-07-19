3a5e34041996ecd32901e2a7a215347378ca7a4e
Lukinhaa <lucasferrarisoares@Gmail.com>
2024-07-19 20:09:38 -0300

    exercício novo

---
+++ Progamacao Funcional/Exercícios de Aula/Ex Soma no AB/AB.rkt
@@ -0,0 +1,38 @@
+#lang racket
+(require examples)
+
+;; Estrutura de um nó de uma arvore binparia
+;; dado é o valor que será armazenado na árvore
+;; sub-arv-esq é um no raiz de uma árvore ou empty
+;; sub-arv-dir é um no raiz de uma árvore ou empty
+
+(struct no (dado sub-arv-esq sub-arv-dir) #:transparent)
+
+(define t1 (no 10 empty empty))
+(define t4 (no 3 empty empty))
+(define t3 (no 8 empty empty))
+
+(define t2(no 9 t1 empty))
+(define t5(no 7 t3 t2))
+(define t6(no 4 t4 empty))
+
+(define t7(no 3 t6 t5))
+
+;; Função que soma todos os nós de uma arvore t
+;; ArvoreBinaria -> número
+
+(examples
+ (check-equal? (soma-arvore t6) 7)
+ (check-equal? (soma-arvore t2) 19)
+ (check-equal? (soma-arvore t3) 8)
+ (check-equal? (soma-arvore t7) 44)
+ (check-equal? (soma-arvore empty) 0))
+
+(define (soma-arvore t)
+  (cond
+    [(empty? t)
+     0]
+    [else
+     (+ (no-dado t)
+        (soma-arvore (no-sub-arv-esq t))
+        (soma-arvore (no-sub-arv-dir t)))]))
